b1e8a14c826a3690e7b33214037361f3d9d9e8e4
jayden <chongjayden07@gmail.com>
2024-10-01 23:54:54 +0800

    works selection

--- src/app/(withfooter)/works/page.tsx
+++ src/app/(withfooter)/works/page.tsx
@@ -163,23 +163,47 @@ function ProjectLista() {
 
 interface HTMLCardElement extends HTMLDivElement {
 	_mouseEnter?: () => void,
-	_mouseLeave?: () => void
+	_mouseLeave?: () => void,
+	_click?: () => void
 }
 
+const CARD_HOVER_GROWTH = 3 // 3% extra from restCardWidth (width for cards at rest)
 function ProjectList() {
 	let [activeIdx, setActiveIdx] = useState(0)
 
 	const [cardWidth, setCardWidth] = useState(0)
 	const [cardHeight, setCardHeight] = useState(0)
-	const [hoverCardIdx, setHoverCardIdx] = useState<number|null>(0)
+	const [hoverCardIdx, setHoverCardIdx] = useState<number|null>(null)
+
+	const [mainCardWidth, setMainCardWidth] = useState(80) // 90% of space
+	const [expandedCardWidth, setExpandedCardWidth] = useState((100 -mainCardWidth -CARD_HOVER_GROWTH) /(ProjectDataOrder[0].length -1) +CARD_HOVER_GROWTH) // remaining space + a bit more
+	const [restCardWidth, setRestCardWidth] = useState((100 -mainCardWidth -(hoverCardIdx == null ? 0 : CARD_HOVER_GROWTH)) /(ProjectDataOrder[0].length -1)) // remaining spce
 
 	const cardsRef = useRef<(HTMLCardElement|null)[]>([])
 	const focusCard = (i: number, card: HTMLCardElement) => {
+		if (card.classList.contains("active")) {
+			return // do nothing
+		}
 		console.log("hover", i)
 		setHoverCardIdx(i) // set space
 	}
 	const unfocusCard = (i: number, card: HTMLCardElement) => {
 		setHoverCardIdx(null) // reset
+	}
+	const clickCard = (i: number, card: HTMLCardElement) => {
+		if (activeIdx === i) {
+			// already clicked
+			return
+		}
+
+		// update state
+		setActiveIdx(i)
+
+		// if hovering over newly selected card, remove hover effect
+		if (hoverCardIdx === i) {
+			// un-hover
+			setHoverCardIdx(null)
+		}
 	}
 	useEffect(() => {
 		if (!cardsRef.current) {
@@ -196,12 +220,15 @@ function ProjectList() {
 			let ii = i++
 			const mouseEnter = () => focusCard(ii, card)
 			const mouseLeave = () => unfocusCard(ii, card)
+			const click = () => clickCard(ii, card)
 
 			card.addEventListener("mouseenter", mouseEnter)
 			card.addEventListener("mouseleave", mouseLeave)
+			card.addEventListener("click", click)
 
 			card._mouseEnter = mouseEnter
 			card._mouseLeave = mouseLeave
+			card._click = click
 		}
 
 		// observe active card to set card width for inner image
@@ -211,6 +238,7 @@ function ProjectList() {
 
 			setCardWidth(rect.width)
 			setCardHeight(rect.width *.5625) // 16:9 aspect ratio
+			console.log("HEIGHT!", rect.width *.5625)
 		})
 		if (cardsRef.current[activeIdx]) {
 			obs.observe(cardsRef.current[activeIdx])
@@ -227,33 +255,75 @@ function ProjectList() {
 				if (card?._mouseLeave) {
 					card?.removeEventListener("mouseleave", card._mouseLeave)
 				}
+				if (card?._click) {
+					card?.removeEventListener("click", card._click)
+				}
 			}
 		}
 	}, [activeIdx, cardsRef])
 
+	useEffect(() => {
+		console.log("hovered", hoverCardIdx)
+		console.log("rest", restCardWidth)
+		console.log("expanded", expandedCardWidth)
+		console.log("total", restCardWidth *(ProjectDataOrder[0].length -1) +expandedCardWidth)
+		setRestCardWidth((100 -mainCardWidth -(hoverCardIdx == null ? 0 : CARD_HOVER_GROWTH)) /(ProjectDataOrder[0].length -1))
+	}, [hoverCardIdx])
+
+	console.log("restCardWidth", restCardWidth)
+
 	return (
-		<div className="relative w-full h-12">
+		<div className="relative w-full">
 			{
 				ProjectDataOrder[0].map((projectId, i) => 
 					<div key={i} ref={el => {cardsRef.current[i] = el}}
 						className={
-							`group relative h-full ${activeIdx === i ? "active" : ""} float-left block`
+							`group relative justify-end h-full ${activeIdx === i ? "active" : ""} overflow-clip float-left block transition-all cursor-pointer`
 						}
 						style={{
 							width: "100%",
-							maxWidth: `${(activeIdx === i ? 10 : (hoverCardIdx === i ? 2 : 1)) /(ProjectDataOrder[0].length +9 +(hoverCardIdx != null ? 1 : 0)) *100}%`,
+							height: cardHeight,
+							maxWidth: `${activeIdx === i ? mainCardWidth : (hoverCardIdx === i ? expandedCardWidth : restCardWidth)}%`,
 							zIndex: ProjectDataOrder[0].length -i
 						}}
 					>
-						<div className="absolute top-0 right-0 z-10 flex flex-col justify-between">
-							<div className="text-right z-10">{i +1}</div>
-							<p className="group-[:not(.active):hover]:block hidden z-10">{ProjectData[projectId].title}</p>
+						<div className="absolute top-0 right-0 p-4 flex flex-col justify-between h-full font-bold text-xl text-white" style={{
+							display: activeIdx === i ? "none" : "flex",
+							zIndex: 3,
+						}}>
+							<div className="text-right">{i +1}</div>
+							<p className="group-[:not(.active):hover]:block hidden z-10 text-accent">{ProjectData[projectId].title}</p>
+						</div>
+						<div className="absolute z-10 flex flex-col items-center justify-end p-2 bg-[#1a1a1ab5] text-white text-2xl font-bold" style={{
+							width: cardWidth,
+							height: cardHeight,
+							display: activeIdx === i ? "flex" : "none",
+							background: "linear-gradient(0deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0) 10%)",
+							zIndex: 2
+						}}>
+							<p>{ProjectData[projectId].title}</p>
+						</div>
+						<div className="absolute top-0 right-0 w-full" style={{
+							height: cardHeight,
+							display: activeIdx === i ? "none" : "block",
+							background: "linear-gradient(90deg, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0) 40%)",
+							zIndex: 3
+						}}>
+						</div>
+						<div className="absolute top-0 right-0 transition-all" style={{
+							width: cardWidth,
+							height: cardHeight,
+							display: activeIdx === i ? "none" : "block",
+							background: hoverCardIdx === i ? "rgba(0,0,0,.1)"
+								: "radial-gradient(circle, rgba(0,0,0,0) 0%, rgba(0,0,0,.4) 100%)",
+							zIndex: 2,
+						}}>
 						</div>
 						<img className="absolute top-0 right-0"
 							style={{
 								width: cardWidth,
 								height: cardHeight,
-								maxWidth: "unset",
+								maxWidth: "unset"
 							}}
 							src={ProjectData[projectId].coverImg.src}/>
 					</div>
