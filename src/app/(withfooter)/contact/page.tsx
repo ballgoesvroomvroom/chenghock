82dd6cfafed2151e156168d5b628594ff2c9fcbc
ballgoesvroomvroom <chongjayden07@gmail.com>
2024-10-03 18:15:50 +0800

    contact page adding scrolling banner above footer
    made page min h svh

--- src/app/(withfooter)/contact/page.tsx
+++ src/app/(withfooter)/contact/page.tsx
@@ -1,15 +1,85 @@
-import { SocialsContainer, SOCIALS_LINKEDIN, SOCIALS_GITHUB, SOCIALS_TELEGRAM, SOCIALS_EMAIL } from "@/app/components/socials";
+"use client"
+
+import { SocialsContainer, SOCIALS_LINKEDIN, SOCIALS_GITHUB, SOCIALS_TELEGRAM, SOCIALS_EMAIL } from "@/app/components/socials"
+import { TopbarContext } from "@/app/components/topbar/topbar"
 
 import quotation_icon from "@/public/icons/quotes.svg"
+import { useContext, useEffect, useRef, useState } from "react"
+
+function ScrollingBanner() {
+	const [textCount, setTextCount] = useState(10)
+	const [textWidth, setTextWidth] = useState(0)
+	const textRef = useRef<HTMLParagraphElement>(null)
+
+	const computeTextCount = () => {
+		if (!textRef.current) {
+			return
+		}
+
+		const rect = textRef.current.getBoundingClientRect()
+		setTextWidth(rect.width)
+		setTextCount(Math.ceil(window.innerWidth /rect.width) +1) // +1 to account for last item
+	}
+
+	useEffect(() => {
+		window.addEventListener("resize", computeTextCount)
+		computeTextCount()
+
+		return () => {
+			window.removeEventListener("resize", computeTextCount)
+		}
+	}, [])
+
+	return (
+		<div id="scrolling-window" className="mt-auto overflow-x-clip opacity-75">
+			<div className="flex gap-8 transition-transform duration-1000 ease-linear" style={{
+				// @ts-ignore
+				"--namebanneroffset": `calc(-${textWidth}px - 2rem)`,
+				animationName: "namebannerlr",
+				animationDuration: "2s",
+				animationIterationCount: "infinite",
+				animationTimingFunction: "linear",
+				animationDirection: "initial"
+			}}>
+				{
+					[...Array(textCount).fill(0)].map((_, i) => {
+						return (<p className="text-2xl font-bold whitespace-nowrap" key={i} ref={i === 0 ? textRef : null}>CHONG CHENG HOCK</p>)
+					})
+				}
+			</div>
+		</div>
+	)
+}
+
+function SocialsCardRow() {
+	return (
+		<div className="flex flex-wrap gap-4 p-4 md:p-8 mt-12 sm:mt-24">
+			{
+				[["LinkedIn", SOCIALS_LINKEDIN, "Chong Cheng Hock"],
+				["GitHub", SOCIALS_GITHUB, "Ballgoesvroomvroom"],
+				["Telegram", SOCIALS_TELEGRAM, "cheng_hock"],
+				["Email", SOCIALS_EMAIL, "hi@chenghock.com"]].map((item, i) => 
+					<a className={`p-3 inline-flex flex-col gap-3 border-solid border-black border-2`} href={item[1]} target="_blank">
+						<p className="font-bold text-lg">{item[0]}</p>
+						<p>{item[2]}</p>
+					</a>
+				)
+			}
+		</div>
+	)
+}
 
 export default function ContactPage() {
+	const { topbarHt, setForceTopbarScrollState } = useContext(TopbarContext)
+
 	return (
-		<div className="flex flex-col p-4 md:p-8 md:pt-24">
-			<div className="flex justify-between gap-48">
+		// @ts-ignore
+		<div className="flex flex-col md:pt-24 sm:min-h-[var(--ht)]" style={{"--ht": `calc(100svh - ${topbarHt}px - 2px)`}}>
+			<div className="flex justify-between gap-48 p-4 md:p-8">
 				<div className="grow">
 					<h1 className="font-bold text-4xl pb-4">Reach out</h1>
-					<p className="pb-2">I am all ears when it comes to receiving constructive feedback and potential collaboration opportunities.</p>
-					<p className="pb-6 md:pb-12">I keep all my socials open for direct messaging, please feel free to reach out to me on whichever channel is your preference.</p>
+					<p className="pb-2">I would love to receive guidance and feedback on any of my works.</p>
+					<p className="pb-6 md:pb-12">I keep all my socials open for direct messaging, please feel free to reach out to me on any channels listed.</p>
 					<SocialsContainer iconSize={48} />
 				</div>
 				<div className="hidden md:flex flex-col gap-2 items-start opacity-40 hover:opacity-100 transition-opacity">
@@ -21,24 +91,8 @@ export default function ContactPage() {
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-wrap gap-4 mt-12 md:mt-24">
-				<a className="p-3 flex flex-col gap-3 border-solid border-black border-2" href={SOCIALS_LINKEDIN} target="_blank">
-					<p className="font-bold text-lg">LinkedIn</p>
-					<p>Chong Cheng Hock</p>
-				</a>
-				<a className="p-3 flex flex-col gap-3 border-solid border-black border-2" href={SOCIALS_GITHUB} target="_blank">
-					<p className="font-bold text-lg">GitHub</p>
-					<p>Ballgoesvroomvroom</p>
-				</a>
-				<a className="p-3 flex flex-col gap-3 border-solid border-black border-2" href={SOCIALS_TELEGRAM} target="_blank">
-					<p className="font-bold text-lg">Telegram</p>
-					<p>cheng_hock</p>
-				</a>
-				<a className="p-3 flex flex-col gap-3 border-solid border-black border-2" href={SOCIALS_EMAIL} target="_blank">
-					<p className="font-bold text-lg">Email</p>
-					<p>hi@chenghock.com</p>
-				</a>
-			</div>
+			<SocialsCardRow />
+			<ScrollingBanner />
 		</div>
 	)
 }
